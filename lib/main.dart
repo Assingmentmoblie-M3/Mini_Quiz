043e82687545ed4f4171466733f474180c3f1c1b
Chhengsean <chhengsean08@gmail.com>
2026-01-26 08:51:05 +0700

    create 2screen for team

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:mini_quiz/pages/admin_side/category_page.dart';
+//import 'package:mini_quiz/pages/admin_side/category_page.dart';
+import 'package:mini_quiz/pages/user_side/test.dart';
 
 void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
+runApp(DevicePreview(builder: (context) => const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mini Quiz',
       debugShowCheckedModeBanner: false,
-      home: const CategoryScreen(),
+      title: 'Quiz App',
+      home: const LevelPage(),
     );
   }
 }
