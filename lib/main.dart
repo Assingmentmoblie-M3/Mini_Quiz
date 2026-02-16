f5c13ec822de60db2a88eb49162cdeb72c420f6b
Alexyong12 <houkionyong@50gmail.com>
2026-02-16 14:01:11 +0700

    update files

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,13 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:mini_quiz/pages/HomeScreen/LoginScren.dart';
-import 'package:mini_quiz/pages/admin_side/admin_dashboard_page.dart';
-import 'package:mini_quiz/pages/HomeScreen/HomeScreen.dart';
-import 'package:mini_quiz/pages/admin_side/view_category_page.dart';
-import 'package:mini_quiz/pages/admin_side/levels_page.dart';
-
+// import 'package:mini_quiz/pages/admin_side/admin_dashboard_page.dart';
+// import 'package:mini_quiz/pages/HomeScreen/HomeScreen.dart';
+// import 'package:mini_quiz/pages/admin_side/view_category_page.dart';
+// import 'package:mini_quiz/pages/admin_side/levels_page.dart';
  void main() {
   runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
@@ -22,10 +20,9 @@ class MyApp extends StatelessWidget {
         textTheme: const TextTheme(
           titleLarge: TextStyle(fontSize: 30, fontWeight: FontWeight.w900, letterSpacing: 1.4),
           bodyMedium: TextStyle(fontSize: 16, ),
-          
         ),
       ),
-      home: const ViewCategoryScreen(),
+      home:const LoginScreen(),
     );
   }
 }
